58f08ca39bd4cf189e6b0f31fa638252c1a63055
giliyablo <gili.yablonka@gmail.com>
2024-06-21 23:43:19 +0300

    api.js

--- src/api.js
+++ src/api.js
@@ -1,13 +1,13 @@
 import axios from 'axios';
 
 const API_URL = 'http://localhost:8080/api'; // Adjust the URL as needed
-r
+
 export const getPersons = () => axios.get(`${API_URL}/persons`);
 export const createPerson = (person) => axios.post(`${API_URL}/persons`, person);
-export const updatePerson = (name, person) => axios.put(`${API_URL}/persons/${name}`, person);
-export const deletePerson = (name) => axios.delete(`${API_URL}/persons/${name}`);
+export const updatePerson = (id, person) => axios.put(`${API_URL}/persons/${id}`, person);
+export const deletePerson = (id) => axios.delete(`${API_URL}/persons/${id}`);
 
 export const getTasks = () => axios.get(`${API_URL}/tasks`);
 export const createTask = (task) => axios.post(`${API_URL}/tasks`, task);
-export const updateTask = (name, task) => axios.put(`${API_URL}/tasks/${name}`, task);
-export const deleteTask = (name) => axios.delete(`${API_URL}/tasks/${name}`);
+export const updateTask = (id, task) => axios.put(`${API_URL}/tasks/${id}`, task);
+export const deleteTask = (id) => axios.delete(`${API_URL}/tasks/${id}`);
